9eaee2a9d3e90ebc01e737c05b3ad2e632eec3ff
cletushunsu <cletusola@outlook.com>
2024-06-16 03:51:11 -0700

    created cart.dart file and cart stateless widget

---
+++ lib/features/cart/ui/cart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget {
+  const Cart({super.key});
+
+  @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
