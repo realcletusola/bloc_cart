2b842e8df3ab81abdb792c6b186ee089c4ab3b85
cletushunsu <cletusola@outlook.com>
2024-06-16 03:58:58 -0700

    initiated events and created method for both homeProductWishlistButtonEvent and homeProductCartButtonEvent. Also created methods for homeProductWishlistNavigateEvent and homeProductCartNavigateEvent

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -5,10 +7,43 @@ import 'home_event.dart';
 import 'home_state.dart';
 
 
+// home bloc class
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  // initiating bloc events
   HomeBloc(): super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
 
-    });
+    on<HomeProductWishlistButtonEvent>(
+      homeProductWishlistButtonMethod);
+
+    on<HomeProductCartButtonEvent>(
+      homeProductCartButtonMethod);
+
+    on<HomeProductWishlistNavigateEvent>(
+        homeProductWishlistNavigateMethod);
+
+    on<HomeProductCartNavigateEvent>(
+        homeProductCartNavigateMethod);
+  }
+
+  // homeProductWishlistButtonMethod method
+  FutureOr<void> homeProductWishlistButtonMethod(
+      HomeProductWishlistButtonEvent event, Emitter<HomeState> emit){
+  }
+
+  // homeProductCartButtonMethod
+  FutureOr<void> homeProductCartButtonMethod(
+      HomeProductCartButtonEvent event, Emitter<HomeState> emit){
+  }
+
+  // homeProductWishlistNavigateMethod
+  FutureOr<void> homeProductWishlistNavigateMethod(
+      HomeProductWishlistNavigateEvent event, Emitter<HomeState> emit){
+      emit(HomeNavigateToWishlistActionState());
+  }
+
+  // homeProductCartNavigateMethod
+  FutureOr<void> homeProductCartNavigateMethod(
+      HomeProductCartNavigateEvent event, Emitter<HomeState> emit){
+      emit(HomeNavigateToCartActionState());
   }
 }
