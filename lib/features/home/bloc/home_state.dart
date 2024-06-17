3e80aa7b846efe868327c8d71d5496f6238faeb8
cletushunsu <cletusola@outlook.com>
2024-06-17 02:58:25 -0700

    modified HomeLoadedSuccessState class to require ProductDataModel

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
+import '../models/home_product.dart';
+
 @immutable
 abstract class HomeState {}
 abstract class HomeActionState extends HomeState{}
@@ -8,7 +10,12 @@ class HomeInitial extends HomeState {}
 
 class HomeLoadingState extends HomeState{}
 
-class HomeLoadedSuccessState extends HomeState{}
+class HomeLoadedSuccessState extends HomeState{
+  final List<ProductDataModel> products;
+  HomeLoadedSuccessState({
+    required this.products,
+  });
+}
 
 class HomeErrorState extends HomeState{}
 
