c4c26da8c756f5b8ac807602a082ffd44a20f649
cletushunsu <cletusola@outlook.com>
2024-06-19 10:28:17 -0700

    created product tile widgets

---
+++ lib/features/home/ui/product_tile_widget.dart
@@ -0,0 +1,68 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_cart/features/home/models/home_product.dart';
+
+class ProductTileWidget extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  const ProductTileWidget({super.key, required this.productDataModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(color: Colors.black)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(productDataModel.imageUrl))
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(productDataModel.name,
+          style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              ),),
+          Text(productDataModel.description,
+          style: const TextStyle(
+            fontSize: 15,
+          ) ,),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text( "\$${productDataModel.price}",
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(onPressed: (){
+                    // homeBloc.add(HomeProductWishlistNavigateEvent());
+                  }, icon: const Icon(Icons.favorite_border)),
+                  IconButton(onPressed: () {
+                    // homeBloc.add(HomeProductCartNavigateEvent());
+                  }, icon: const Icon(Icons.shopping_bag_outlined))
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
