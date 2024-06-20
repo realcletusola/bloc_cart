70746976e8053034e5aab493ef9d8524b9c4c051
cletushunsu <cletusola@outlook.com>
2024-06-20 00:46:28 -0700

    updated button event for both cart and wishlist

--- lib/features/home/bloc/home_event.dart
+++ lib/features/home/bloc/home_event.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_cart/features/home/models/home_product.dart';
 
 @immutable
 abstract class HomeEvent {}
@@ -6,11 +7,17 @@ abstract class HomeEvent {}
 class HomeInitialEvent extends HomeEvent{}
 
 class HomeProductWishlistButtonEvent extends HomeEvent{
-
+  final ProductDataModel clickedProduct;
+  HomeProductWishlistButtonEvent({
+    required this.clickedProduct,
+});
 }
 
 class HomeProductCartButtonEvent extends HomeEvent{
-
+  final ProductDataModel clickedProduct;
+  HomeProductCartButtonEvent({
+   required this.clickedProduct,
+});
 }
 
 class HomeProductWishlistNavigateEvent extends HomeEvent{
