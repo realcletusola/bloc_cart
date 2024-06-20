3c2281eaa5fced85f32ce1624960dfd75bdc023a
cletushunsu <cletusola@outlook.com>
2024-06-20 02:58:38 -0700

    created cart functionality

--- lib/features/cart/ui/cart.dart
+++ lib/features/cart/ui/cart.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cart/features/cart/bloc/cart_bloc.dart';
+import 'package:flutter_cart/features/cart/bloc/cart_event.dart';
+import 'package:flutter_cart/features/cart/ui/cart_tile_widget.dart';
+
+import '../bloc/cart_state.dart';
+
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -8,10 +15,44 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  final CartBloc cartBloc = CartBloc();
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart Items'),
+        backgroundColor: Colors.blue,
+      ),
+      body: BlocConsumer<CartBloc, CartState> (
+        bloc: cartBloc,
+        listener: (context, state) {
 
+      },
+      listenWhen: (previous, current) => current is CartActionState,
+      buildWhen: (previous, current) => current is !CartActionState,
+      builder: (context, state){
+          switch (state.runtimeType) {
+            case CartSuccessState:
+              final successState = state as CartSuccessState;
+              return ListView.builder(
+
+                    itemCount: successState.cartItems.length,
+                    itemBuilder: (context, index) {
+                    return CartTileWidget(
+                    cartBloc: cartBloc,
+                    productDataModel: successState.cartItems[index]);
+                    });
+
+            default:
+          }
+           return Container();
+      },
+    ),
     );
   }
 }
